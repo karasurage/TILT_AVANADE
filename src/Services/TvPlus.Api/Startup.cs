d9d69e563f76335dd1745e1852658b8a8e7c9421
karasurage <nicholas.mateus@gmail.com>
2020-10-18 17:01:39 -0300

    18_10_2020__012_Swagger

--- src/Services/TvPlus.Api/Startup.cs
+++ src/Services/TvPlus.Api/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -9,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using TvPlus.Infrastructure.Ioc;
 
 namespace TvPlus.Api
@@ -25,10 +27,38 @@ namespace TvPlus.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-         
-            services.AddOptions();
             services.AddControllers();
             services.AddOptions();
+
+            #region AddCors
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                        builder =>
+                        builder.AllowAnyOrigin()
+                               .AllowAnyMethod()
+                               .AllowAnyHeader());
+            });
+
+            #endregion
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "TvPlus",
+                        Version = "v1",
+                        Description = $"Api de TvPlus",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "TvPlus_Project",
+                            Url = new Uri("https://github.com/karasurage/TILT_AVANADE_BACK_END.git")
+                        }
+                    });
+            });
+
             RegisterServices(services);
 
         }
@@ -43,13 +73,16 @@ namespace TvPlus.Api
 
             app.UseRouting();
             app.UseCors("CorsPolicy");
-            app.UseAuthentication();
-            app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TvPlus");
+            });
         }
 
         void RegisterServices(IServiceCollection services)
